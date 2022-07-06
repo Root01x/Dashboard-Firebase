95626bfda8d5d8c432a3dd481fcda82299f534ca
Dave Rodriguez <root01x@gmail.com>
2022-07-05 22:06:31 -0500

    login

---
+++ src/js/modules/auth.js
@@ -0,0 +1,10 @@
+// export default function hello () {
+//     alert("hola mundo2");
+// } 
+
+// alert("VERSION220;");
+// let date = new Date()
+// function newF(){console.log(date)}
+console.log('asdasd')
+
+// export default newF
